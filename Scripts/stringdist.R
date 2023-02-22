b2d5d9b8c5bd2a5fd6e80c9fb595e9cb5717599c
luis-escalante <107579035+luis-escalante@users.noreply.github.com>
2023-02-21 23:06:50 -0600

    update to stringdist.r script

--- Scripts/stringdist.R
+++ Scripts/stringdist.R
@@ -9,7 +9,8 @@ new_total_corp<-NULL
 
 #i<-4475
 #y<-2018
-for(y in 2018:2019){
+#for(y in 2018:2019){       (#used on 16 February)
+for(y in 2018:2022){ #CHANGE YEAR
   names_corps<-a[!duplicated(py_owner_name) & year==y,]
   names_corps[,py_owner_name_std:=trimws(py_owner_name)]
   for(i in 1:dim(names_corps)[1]){
@@ -18,9 +19,10 @@ for(y in 2018:2019){
     names_corps[stringdist_Var<=5 & ini==T,new_corp_name:=names_corps$py_owner_name_std[i]]
   }  
   new_total_corp<-rbind(new_total_corp,names_corps)
+  fwrite(x = new_total_corp,file = "../BCAD Data/total_corp_housing_std.csv")
+  
 }
 
-fwrite(x = new_total_corp,file = "../BCAD Data/total_corp_housing_std.csv")
 
 uniqueN(names_corps$py_owner_name_std)
 uniqueN(names_corps$new_corp_name)
@@ -33,7 +35,16 @@ library(ggplot2)
 names_corps_sum<-names_corps[,.(value=sum(TotalCLProp,na.rm=T)),by=.(year,new_corp_name)]
 setkeyv(names_corps_sum,c("year","value"))
 
-ggplot(data = tail(names_corps_sum,20))+
+K<-ggplot(data = tail(names_corps_sum,20))+
   geom_bar(aes(x=reorder(new_corp_name,value),y=value),stat = "identity")+
-  coord_flip()
+  coord_flip()+
+  labs(title = "Top 20 Corporate Landlords by Total Properties Owned in Bexar County in 2018-2022", #CHANGE YEAR
+       y = "Corporate Landlord Names",
+       x = "Number of Total Properties")
+
+  ggsave("Outputs/Top 20_2018_2022.png", width=12, height=9) #CHANGE YEAR and WIDTH HEIGHT BASED ON PLOT.
+  
+
+
+
 
